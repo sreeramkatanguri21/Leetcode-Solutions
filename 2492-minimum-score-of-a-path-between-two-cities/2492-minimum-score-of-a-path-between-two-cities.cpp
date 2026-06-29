f0eb5886f4af8d2536f34a31bbea5b7209c4c499
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-06-29 18:26:26 +0530

    Time: 105 ms (28.51%), Space: 146.1 MB (14.57%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,29 +1,55 @@
 class Solution {
 public:
+    void dfs(int src, int n, vector<vector<pair<int,int>>>& adj, vector<int>& vis, int& ans) {
+        vis[src] = 1;
+
+        for(auto [v, wt]: adj[src]) {
+            if(!vis[v]) {
+                dfs(v, n, adj, vis, ans);
+            }
+            ans = min(ans, wt);
+        }
+    }
     int minScore(int n, vector<vector<int>>& roads) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>> pq;
         vector<vector<pair<int,int>>> adj(n);
         for(auto e: roads) {
             adj[e[0]-1].push_back({e[1]-1, e[2]});
             adj[e[1]-1].push_back({e[0]-1, e[2]});
         }
-        vector<int> dist(n, INT_MAX);
-        pq.push({INT_MAX, 0});
+        vector<int> vis(n, 0);
+        int ans = INT_MAX;
+        dfs(0, n, adj, vis, ans);
+        return ans;
 
-        while(!pq.empty()) {
-            auto [d, u] = pq.top();
-            pq.pop();
 
-            if(d > dist[u]) continue;
 
-            for(auto [v, wt]: adj[u]) {
-                if(min(dist[u], wt) < dist[v]) {
-                    dist[v] = min(dist[u], wt);
-                    pq.push({dist[v], v});
-                }
-            }
-        }
 
-        return dist[n-1];
+
+
+
+        // priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        // vector<vector<pair<int,int>>> adj(n);
+        // for(auto e: roads) {
+        //     adj[e[0]-1].push_back({e[1]-1, e[2]});
+        //     adj[e[1]-1].push_back({e[0]-1, e[2]});
+        // }
+        // vector<int> dist(n, INT_MAX);
+        // pq.push({INT_MAX, 0});
+
+        // while(!pq.empty()) {
+        //     auto [d, u] = pq.top();
+        //     pq.pop();
+
+        //     if(d > dist[u]) continue;
+
+        //     for(auto [v, wt]: adj[u]) {
+        //         if(min(dist[u], wt) < dist[v]) {
+        //             dist[v] = min(dist[u], wt);
+        //             pq.push({dist[v], v});
+        //         }
+        //     }
+        // }
+
+        // return dist[n-1];
     }
 };
