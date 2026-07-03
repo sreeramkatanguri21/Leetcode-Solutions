0070d103f09509e2e38a8acef69f69cd6d4e163d
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-07-03 22:09:54 +0530

    Time: 0 ms (100%), Space: 77 MB (74.86%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,44 +10,46 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* prev = NULL;
-        ListNode* curr = head;
+    // ListNode* reverseList(ListNode* head) {
+    //     ListNode* prev = NULL;
+    //     ListNode* curr = head;
         
-        while(curr) {
-            ListNode* forward = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = forward;
-        }
+    //     while(curr) {
+    //         ListNode* forward = curr->next;
+    //         curr->next = prev;
+    //         prev = curr;
+    //         curr = forward;
+    //     }
         
-        return prev;
-    }
-    void insertAtTail(int val, ListNode* &head, ListNode* &tail) {
-        ListNode* newNode = new ListNode(val);
+    //     return prev;
+    // }
+    // void insertAtTail(int val, ListNode* &head, ListNode* &tail) {
+    //     ListNode* newNode = new ListNode(val);
 
-        if(head == NULL) {
-            head = newNode;
-            tail = newNode;
-        }
-        else {
-            tail->next = newNode;
-            tail = newNode;
-        }
-    }
+    //     if(head == NULL) {
+    //         head = newNode;
+    //         tail = newNode;
+    //     }
+    //     else {
+    //         tail->next = newNode;
+    //         tail = newNode;
+    //     }
+    // }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* temp1 = l1;
         ListNode* temp2 = l2;
         int carry = 0;
-        ListNode* head = NULL;
-        ListNode* tail = NULL;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
         while(temp1 || temp2 || carry != 0) {
             int a = temp1!=NULL?temp1->val:0;
             int b = temp2!=NULL?temp2->val:0;
             
             int sum = a + b + carry;
             int val = sum%10;
-            insertAtTail(val, head, tail);
+            ListNode* newNode = new ListNode(val);
+            temp->next = newNode;
+            temp = newNode;
 
             carry = sum/10;
             temp1 = (temp1!=NULL)?temp1->next:temp1;
@@ -55,6 +57,6 @@ public:
 
         }
 
-        return head;
+        return dummy->next;
     }
 };
