f60e78713dfaa24b8f941f270f4ee7ec580129a2
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-07-04 22:43:37 +0530

    Time: 1 ms (45.38%), Space: 77.2 MB (12.49%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -35,28 +35,56 @@ public:
     //         tail = newNode;
     //     }
     // }
+    void solve(ListNode* l1, ListNode* l2, int carry, ListNode* &head, ListNode* &tail) {
+        if(!l1 && !l2 && carry == 0) return;
+
+        int a = l1?l1->val:0;
+        int b = l2?l2->val:0;
+
+        int sum = a + b + carry;
+        int digit = sum%10;
+        carry = sum/10;
+
+        tail->next = new ListNode(digit);
+        tail = tail->next;
+
+        l1 = l1?l1->next:l1;
+        l2 = l2?l2->next:l2;
+
+        solve(l1, l2, carry, head, tail);
+
+
+    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* temp1 = l1;
-        ListNode* temp2 = l2;
-        int carry = 0;
-        ListNode* dummy = new ListNode(-1);
-        ListNode* temp = dummy;
-        while(temp1 || temp2 || carry != 0) {
-            int a = temp1!=NULL?temp1->val:0;
-            int b = temp2!=NULL?temp2->val:0;
+        ListNode* head = new ListNode(-1);
+        ListNode* tail = head;
+
+        solve(l1, l2, 0, head, tail);
+
+        return head->next;
+
+
+        // ListNode* temp1 = l1;
+        // ListNode* temp2 = l2;
+        // int carry = 0;
+        // ListNode* dummy = new ListNode(-1);
+        // ListNode* temp = dummy;
+        // while(temp1 || temp2 || carry != 0) {
+        //     int a = temp1!=NULL?temp1->val:0;
+        //     int b = temp2!=NULL?temp2->val:0;
             
-            int sum = a + b + carry;
-            int val = sum%10;
-            ListNode* newNode = new ListNode(val);
-            temp->next = newNode;
-            temp = newNode;
+        //     int sum = a + b + carry;
+        //     int val = sum%10;
+        //     ListNode* newNode = new ListNode(val);
+        //     temp->next = newNode;
+        //     temp = newNode;
 
-            carry = sum/10;
-            temp1 = (temp1!=NULL)?temp1->next:temp1;
-            temp2 = (temp2!=NULL)?temp2->next:temp2;
+        //     carry = sum/10;
+        //     temp1 = (temp1!=NULL)?temp1->next:temp1;
+        //     temp2 = (temp2!=NULL)?temp2->next:temp2;
 
-        }
+        // }
 
-        return dummy->next;
+        // return dummy->next;
     }
 };
