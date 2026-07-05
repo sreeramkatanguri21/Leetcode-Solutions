2d8d702a157dae1f5cd1c516dc2aec484bab44cd
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-07-06 01:04:23 +0530

    Time: 0 ms (100%), Space: 15.6 MB (28.14%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,33 +11,51 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        ListNode* odd = new ListNode(-1);
-        ListNode* oddTemp = odd;
-        ListNode* even = new ListNode(-1);
-        ListNode* evenTemp = even;
+        if(head == NULL || head->next == NULL || head->next->next == NULL)  return head;
 
-        bool isOdd = true;
-        ListNode* temp = head;
-        while(temp) {
-            ListNode* forward = temp->next;
-            temp->next = NULL;
+        ListNode* odd = head;
+        ListNode* evenHead = head->next;
+        ListNode* even = head->next;
 
-            if(isOdd) {
-                oddTemp->next = temp;
-                oddTemp = temp;
-            }
-            else {
-                evenTemp->next = temp;
-                evenTemp = temp;
-            }
-
-            isOdd = !isOdd;
-            temp = forward;
+        while(even && even->next) {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
         }
 
-        oddTemp->next = even->next;
+        odd->next = evenHead;
 
         return head;
+
+
+        // ListNode* odd = new ListNode(-1);
+        // ListNode* oddTemp = odd;
+        // ListNode* even = new ListNode(-1);
+        // ListNode* evenTemp = even;
+
+        // bool isOdd = true;
+        // ListNode* temp = head;
+        // while(temp) {
+        //     ListNode* forward = temp->next;
+        //     temp->next = NULL;
+
+        //     if(isOdd) {
+        //         oddTemp->next = temp;
+        //         oddTemp = temp;
+        //     }
+        //     else {
+        //         evenTemp->next = temp;
+        //         evenTemp = temp;
+        //     }
+
+        //     isOdd = !isOdd;
+        //     temp = forward;
+        // }
+
+        // oddTemp->next = even->next;
+
+        // return head;
 
     }
 };
