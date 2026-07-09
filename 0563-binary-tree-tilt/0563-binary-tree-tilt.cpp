f2523d4a5791f51c6ae7449558362ed098afefa8
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-07-09 15:36:32 +0530

    Time: 0 ms (100%), Space: 23.7 MB (49.06%) - LeetHub

--- 0563-binary-tree-tilt/0563-binary-tree-tilt.cpp
+++ 0563-binary-tree-tilt/0563-binary-tree-tilt.cpp
@@ -25,7 +25,17 @@ public:
 
         return val + left + right;
     }
+    pair<int, int> solve2(TreeNode* root) {
+        if(!root) return {0, 0};
+
+        auto left = solve2(root->left);
+        auto right = solve2(root->right);
+        int val = abs(left.second-right.second);
+
+        return {val + left.first + right.first, root->val+left.second+right.second};
+    }
     int findTilt(TreeNode* root) {
-        return solve(root);
+        auto ans = solve2(root);
+        return ans.first;
     }
 };
