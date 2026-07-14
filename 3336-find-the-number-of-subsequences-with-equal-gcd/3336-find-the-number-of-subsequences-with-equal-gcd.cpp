70b723ebd2f32bf799dc8f125073de284f5ef83e
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-07-14 12:45:01 +0530

    Time: 1214 ms (31.3%), Space: 35.5 MB (86.09%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -40,12 +40,38 @@ public:
 
         return dp[0][0][0];
     }
+    int solveTabSO(vector<int>& nums) {
+        int n = nums.size();
+        int maxi = *max_element(begin(nums), end(nums));
+        //dp.resize(n+1, vector<vector<int>>(maxi+1, vector<int>(maxi+1, 0)));
+        vector<vector<int>> curr(maxi+1, vector<int>(maxi+1, 0));
+        vector<vector<int>> next(maxi+1, vector<int>(maxi+1, 0));
+
+        for(int i=1; i<=maxi; i++) {
+            next[i][i] = 1;
+        }
+        for(int i=n-1; i>=0; i--) {
+            for(int gcd1=maxi; gcd1>=0; gcd1--) {
+                for(int gcd2=maxi; gcd2>=0; gcd2--) {
+                    int op1 = next[(gcd1==0)?nums[i]:__gcd(gcd1, nums[i])][gcd2]%MOD;
+                    int op2 = next[gcd1][(gcd2 == 0)?nums[i]:__gcd(nums[i], gcd2)]%MOD;
+                    int op3 = next[gcd1][gcd2]%MOD;
+
+                    curr[gcd1][gcd2] = ((op1 + op2)%MOD + op3)%MOD;
+                }
+            }
+            next = curr;
+        }
+
+
+        return next[0][0];
+    }
     int subsequencePairCount(vector<int>& nums) {
         // int n = nums.size();
         // int maxi = *max_element(begin(nums), end(nums));
         // dp.resize(n+1, vector<vector<int>>(maxi+1, vector<int>(maxi+1, -1)));
         // return solve(nums, 0, 0, 0);
 
-        return solveTab(nums);
+        return solveTabSO(nums);
     }
 };
