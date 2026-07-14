20d5a21d3fae2fe945bc1696eec43d18bb1d6c14
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-07-14 12:42:00 +0530

    Time: 1398 ms (13.91%), Space: 296.3 MB (26.09%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -4,23 +4,48 @@ public:
     vector<vector<vector<int>>> dp;
     int solve(vector<int>& nums, int i, int gcd1, int gcd2) {
         if(i >= nums.size()) {
-            if(gcd1 != -1 && gcd2 != -1) return gcd1 == gcd2;
+            if(gcd1 != 0 && gcd2 != 0) return gcd1 == gcd2;
             return 0;
         }
 
-        if(dp[i][gcd1+1][gcd2+1] != -1) return dp[i][gcd1+1][gcd2+1];
+        if(dp[i][gcd1][gcd2] != -1) return dp[i][gcd1][gcd2];
 
 
-        int op1 = solve(nums, i+1, (gcd1==-1)?nums[i]:__gcd(gcd1, nums[i]), gcd2)%MOD;
-        int op2 = solve(nums, i+1, gcd1, (gcd2 == -1)?nums[i]:__gcd(nums[i], gcd2))%MOD;
+        int op1 = solve(nums, i+1, (gcd1==0)?nums[i]:__gcd(gcd1, nums[i]), gcd2)%MOD;
+        int op2 = solve(nums, i+1, gcd1, (gcd2 == 0)?nums[i]:__gcd(nums[i], gcd2))%MOD;
         int op3 = solve(nums, i+1, gcd1, gcd2)%MOD;
 
-        return dp[i][gcd1+1][gcd2+1] = ((op1 + op2)%MOD + op3)%MOD;
+        return dp[i][gcd1][gcd2] = ((op1 + op2)%MOD + op3)%MOD;
     }
-    int subsequencePairCount(vector<int>& nums) {
+    int solveTab(vector<int>& nums) {
         int n = nums.size();
         int maxi = *max_element(begin(nums), end(nums));
-        dp.resize(n, vector<vector<int>>(maxi+2, vector<int>(maxi+2, -1)));
-        return solve(nums, 0, -1, -1);
+        dp.resize(n+1, vector<vector<int>>(maxi+1, vector<int>(maxi+1, 0)));
+
+        for(int i=1; i<=maxi; i++) {
+            dp[n][i][i] = 1;
+        }
+        for(int i=n-1; i>=0; i--) {
+            for(int gcd1=maxi; gcd1>=0; gcd1--) {
+                for(int gcd2=maxi; gcd2>=0; gcd2--) {
+                    int op1 = dp[i+1][(gcd1==0)?nums[i]:__gcd(gcd1, nums[i])][gcd2]%MOD;
+                    int op2 = dp[i+1][gcd1][(gcd2 == 0)?nums[i]:__gcd(nums[i], gcd2)]%MOD;
+                    int op3 = dp[i+1][gcd1][gcd2]%MOD;
+
+                    dp[i][gcd1][gcd2] = ((op1 + op2)%MOD + op3)%MOD;
+                }
+            }
+        }
+
+
+        return dp[0][0][0];
+    }
+    int subsequencePairCount(vector<int>& nums) {
+        // int n = nums.size();
+        // int maxi = *max_element(begin(nums), end(nums));
+        // dp.resize(n+1, vector<vector<int>>(maxi+1, vector<int>(maxi+1, -1)));
+        // return solve(nums, 0, 0, 0);
+
+        return solveTab(nums);
     }
 };
