27bd32ae5cad2a9fd9b8b10a168ee8c8c3266d44
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-07-19 00:54:07 +0530

    Time: 25 ms (70.66%), Space: 32.1 MB (93.84%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -12,35 +12,109 @@ public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if(!root) return "N";
+        queue<TreeNode*> q;
+        q.push(root);
 
-        return to_string(root->val) + "," + serialize(root->left) + "," + serialize(root->right);
+        string str = "";
+        while(!q.empty()) {
+            auto temp = q.front();
+            q.pop();
+
+            if(!str.empty()) str.push_back(',');
+            if(temp) {
+                str += to_string(temp->val);
+                q.push(temp->left);
+                q.push(temp->right);
+            }
+            else {
+                str.push_back('N');
+            }
+        }
+
+
+        return str;
     }
 
-    TreeNode* solve(string& data, int& i) {
-        if(i >= data.length()) return NULL;
-
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if(data.length() < 3) return NULL;
+        
+        queue<TreeNode*> q;
+        int i = 0;
         string curr = "";
         while(i < data.length() && data[i] != ',') {
             curr.push_back(data[i++]);
         }
         i++;
-        
-        if(curr == "N") return NULL;
-        
         int val = stoi(curr);
         TreeNode* root = new TreeNode(val);
-        root->left = solve(data, i);
-        root->right = solve(data, i);
+        q.push(root);
+
+        while(!q.empty()) {
+            auto temp = q.front();
+            q.pop();
+
+            string curr = "";
+            while(i < data.length() && data[i] != ',') {
+                curr.push_back(data[i++]);
+            }
+            i++;
+
+            if(curr != "N") {
+                int val = stoi(curr);
+                TreeNode* left = new TreeNode(val);
+                temp->left = left;
+                q.push(left);
+            }
+
+            curr = "";
+            while(i < data.length() && data[i] != ',') {
+                curr.push_back(data[i++]);
+            }
+            i++;
+
+            if(curr != "N") {
+                int val = stoi(curr);
+                TreeNode* right = new TreeNode(val);
+                temp->right = right;
+                q.push(right);
+            }
+        }
 
         return root;
+    }
 
-    }
-    // Decodes your encoded data to tree.
-    TreeNode* deserialize(string data) {
-        int i = 0;
-        return solve(data, i);
-    }
+
+    // // Encodes a tree to a single string.
+    // string serialize(TreeNode* root) {
+    //     if(!root) return "N";
+
+    //     return to_string(root->val) + "," + serialize(root->left) + "," + serialize(root->right);
+    // }
+
+    // TreeNode* solve(string& data, int& i) {
+    //     if(i >= data.length()) return NULL;
+
+    //     string curr = "";
+    //     while(i < data.length() && data[i] != ',') {
+    //         curr.push_back(data[i++]);
+    //     }
+    //     i++;
+
+    //     if(curr == "N") return NULL;
+        
+    //     int val = stoi(curr);
+    //     TreeNode* root = new TreeNode(val);
+    //     root->left = solve(data, i);
+    //     root->right = solve(data, i);
+
+    //     return root;
+    // }
+    // // Decodes your encoded data to tree.
+    // TreeNode* deserialize(string data) {
+    //     int i = 0;
+    //     return solve(data, i);
+    // }
 };
 
 // Your Codec object will be instantiated and called as such:
