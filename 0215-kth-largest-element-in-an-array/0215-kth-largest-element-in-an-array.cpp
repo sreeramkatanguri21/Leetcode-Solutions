5fa9fac08bd8de2b36adb873b431757842da5a0e
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-08-11 15:48:15 +0530

    Time: 2195 ms (5%), Space: 69.1 MB (91.77%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,22 +1,55 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        //Approach-3 -> Min Heap
-        priority_queue<int, vector<int>, greater<>> pq;
-
-        for(auto num: nums) {
-            if(pq.size() < k) {
-                pq.push(num);
-            }
-            else {
-                if(num > pq.top()) {
-                    pq.pop();
-                    pq.push(num);
-                }
+    int partition(vector<int>& nums, int s, int e) {
+        int k = e;
+        int pivot = nums[s];
+        for(int i=e; i>s; i--) {
+            if(nums[i] > pivot) {
+                swap(nums[i], nums[k]);
+                k--;
             }
         }
+        swap(nums[s], nums[k]);
 
-        return pq.top();
+        return k;
+    }
+    int solve(vector<int>& nums, int k, int s, int e, int n) {
+        if(s == e) return nums[s];
+        if(s > e) return -1;
+
+        int pi = partition(nums, s, e);
+        if(pi == n-k) return nums[pi];
+        if(n-k < pi) {
+            return solve(nums, k, s, pi-1, n);
+        }
+        else {
+            return solve(nums, k, pi+1, e, n);
+        }
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        // Approach-4 -> Quicksort
+        int n = nums.size();
+        return solve(nums, k, 0, n-1, n);
+
+
+
+
+        //Approach-3 -> Min Heap
+        // priority_queue<int, vector<int>, greater<>> pq;
+
+        // for(auto num: nums) {
+        //     if(pq.size() < k) {
+        //         pq.push(num);
+        //     }
+        //     else {
+        //         if(num > pq.top()) {
+        //             pq.pop();
+        //             pq.push(num);
+        //         }
+        //     }
+        // }
+
+        // return pq.top();
 
 
         //Approach-2 -> Max Heap
