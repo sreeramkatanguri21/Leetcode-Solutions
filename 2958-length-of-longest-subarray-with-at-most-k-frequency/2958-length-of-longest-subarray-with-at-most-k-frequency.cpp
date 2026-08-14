2dcc9751b12623cbba6fed129a140cf0b1bd9223
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-08-14 09:52:29 +0530

    Time: 53 ms (89.1%), Space: 149.4 MB (30.22%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
-        int l = 0;
-        int r =  0;
+
         int n = nums.size();
-        unordered_map<int, int> freq;
-        int maxLen = 0;
+        int ans = 0;
+        int l = 0;
+        int r = 0;
+        int cnt = 0;
+        unordered_map<int, int> mpp;
         while(r < n) {
-            freq[nums[r]]++;
-            while(freq[nums[r]] > k) {
-                freq[nums[l]]--;
+            mpp[nums[r]]++;
+            if(mpp[nums[r]] > k) cnt++;
+            if(cnt > 0) {
+                if(mpp[nums[l]] > k) cnt--;
+                mpp[nums[l]]--;
                 l++;
             }
 
-            maxLen = max(maxLen, r-l+1);
+            ans = max(ans, r-l+1);
             r++;
         }
 
-        return maxLen;
+        return ans;
+
+        // int l = 0;
+        // int r =  0;
+        // int n = nums.size();
+        // unordered_map<int, int> freq;
+        // int maxLen = 0;
+        // while(r < n) {
+        //     freq[nums[r]]++;
+        //     while(freq[nums[r]] > k) {
+        //         freq[nums[l]]--;
+        //         l++;
+        //     }
+
+        //     maxLen = max(maxLen, r-l+1);
+        //     r++;
+        // }
+
+        // return maxLen;
     }
 };
