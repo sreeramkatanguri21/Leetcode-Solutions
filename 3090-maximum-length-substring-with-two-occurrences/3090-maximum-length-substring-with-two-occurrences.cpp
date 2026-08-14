cd177011a274c30a8b09f858adef1563cd376336
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-08-14 09:39:59 +0530

    Time: 0 ms (100%), Space: 9.7 MB (44.69%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -3,16 +3,38 @@ public:
     int maximumLengthSubstring(string s) {
         int n = s.length();
         int ans = 0;
-        for(int i=0; i<n; i++) {
-            unordered_map<int,int> mpp;
-            for(int j=i; j<n; j++) {
-                mpp[s[j]]++;
-                if(mpp[s[j]] > 2) break;
-
-                ans = max(ans, j-i+1);
+        int l = 0;
+        int r = 0;
+        unordered_map<char, int> mpp;
+        while(r < n) {
+            mpp[s[r]]++;
+            
+            while(mpp[s[r]] > 2) {
+                mpp[s[l]]--;
+                l++;
             }
+
+
+            ans = max(ans, r-l+1);
+            r++;
         }
 
         return ans;
+
+
+
+        // int n = s.length();
+        // int ans = 0;
+        // for(int i=0; i<n; i++) {
+        //     unordered_map<int,int> mpp;
+        //     for(int j=i; j<n; j++) {
+        //         mpp[s[j]]++;
+        //         if(mpp[s[j]] > 2) break;
+
+        //         ans = max(ans, j-i+1);
+        //     }
+        // }
+
+        // return ans;
     }
 };
