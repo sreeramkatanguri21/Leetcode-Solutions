81baffcd7be995a432aea7eb287b2f5cdcea7f69
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-08-19 15:07:32 +0530

    Time: 3 ms (63.07%), Space: 18.3 MB (8.55%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,35 @@
+class Compare {
+    public:
+        bool operator()(auto& a, auto& b) {
+            if(a.first == b.first ) {
+                return a.second > b.second;
+            }
+
+            return a.first < b.first;
+        }
+};
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        priority_queue<pair<int,string>, vector<pair<int,string>>, Compare> pq;
+
+        unordered_map<string, int> mpp;
+        for(auto word: words) {
+            mpp[word]++;
+        }
+
+        for(auto [s, f]: mpp) {
+            pq.push({f, s});
+        }
+
+        vector<string> ans;
+        while(!pq.empty()) {
+            auto [f, s] = pq.top();
+            pq.pop();
+            ans.push_back(s);
+            if(ans.size() == k) break;
+        }
+
+        return ans;
+    }
+};
