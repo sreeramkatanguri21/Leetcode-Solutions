1ee34097a324bd5c38fa051f62326ccaa3bf8c9e
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-08-19 13:36:32 +0530

    Time: 167 ms (6.76%), Space: 122.6 MB (5.07%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    unordered_map<int, unordered_set<int>> mpp;
+    bool check(unordered_set<int>& st, int s, int e) {
+        for(int i=s; i<=e; i++) {
+            if(st.count(i)) return false;
+        }
+
+        return true;
+    }
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+
+        int cnt = 0;
+        int m = reservedSeats.size();
+        for(int i=0; i<m; i++) {
+            auto v = reservedSeats[i];
+            mpp[v[0]].insert(v[1]);
+        }
+        // for(int i=1; i<=n; i++) {
+        //     if(!mpp.count(i)) {
+        //         cnt += 2;
+        //         continue;
+        //     }
+        //     if(check(i, 2, 9)) {
+        //             cnt += 2;
+        //     }
+        //     else if(check(i, 2, 5) || check(i, 4, 7) || check(i, 6, 9)) {
+        //         cnt++;
+        //     }
+        // }
+        int rowCount = 0;
+        for(auto [r, st]: mpp) {
+            rowCount++;
+            
+            if(check(st, 2, 9)) {
+                    cnt += 2;
+            }
+            else if(check(st, 2, 5) || check(st, 4, 7) || check(st, 6, 9)) {
+                cnt++;
+            }
+
+        }
+
+        return cnt + (n-rowCount)*2;
+
+
+        // int cnt = 0;
+        // ranges::sort(reservedSeats);
+        // int j = 0;
+        // int m = reservedSeats.size();
+        // for(int i=1; i<=n; i++) {
+        //     if(j < m && reservedSeats[j][0] == i) {
+        //         unordered_set<int> st;
+        //         while(j < m && reservedSeats[j][0] == i) {
+        //             st.insert(reservedSeats[j++][1]);
+        //         }
+        //         if(check(st, 2, 9)) {
+        //             cnt += 2;
+        //         }
+        //         else if(check(st, 2, 5) || check(st, 4, 7) || check(st, 6, 9)) {
+        //             cnt++;
+        //         }
+        //     }
+        //     else {
+        //         cnt += 2;
+        //     }
+        // }
+
+        // return cnt;
+    }
+};
