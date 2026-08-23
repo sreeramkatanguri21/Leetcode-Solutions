ccb9a699a3e0908885f27309e9d182234b5b18b8
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-08-23 12:35:10 +0530

    Time: 2 ms (71.33%), Space: 13.9 MB (92.07%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.length();
+        int q1 = 0, q2 = 0;
+        int sum1 = 0, sum2 = 0;
+        for(int i=0; i<n/2; i++) {
+            if(num[i] == '?') q1++;
+            else sum1 += num[i]-'0';
+        }
+
+        for(int i=n/2; i<n; i++) {
+            if(num[i] == '?') q2++;
+            else sum2 += num[i]-'0';
+        }
+
+        if(q1 > q2) {
+            int diffSum = sum2 - sum1;
+            int diffQ = q1-q2;
+            if(diffSum < 0) return true;
+            if(diffQ & 1) return true;
+            else if(diffQ%2 == 0 && diffSum == diffQ/2*9) return false;
+            return true;
+        }
+        else if(q2 > q1) {
+            int diffSum = sum1 - sum2;
+            int diffQ = q2-q1;
+            if(diffSum < 0) return true;
+            if(diffQ & 1) return true;
+            else if(diffQ%2 == 0 && diffSum == diffQ/2*9) return false;
+            return true;
+        }
+        else {
+            return sum1 != sum2;
+        }
+    }
+};
