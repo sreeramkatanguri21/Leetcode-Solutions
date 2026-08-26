77a2140d42c593378702ff70d5c0b51774129d7f
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-08-26 09:50:30 +0530

    Time: 0 ms (100%), Space: 8.7 MB (82.87%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,25 +1,57 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-
         int n = s.length();
         string ans = "";
-        for(int i=0; i<n; i++) {
-            string str = "";
-            int cnt = 0;
-            for(int j=i; j<n; j++) {
-                str.push_back(s[j]);
-                if(s[j] == '1') cnt++;
-                if(cnt == k) {
-                    if(ans.length() == 0 || 
-                        str.length() < ans.length() || 
-                        (str.length() == ans.length() && str < ans)) {
-                        ans = str;
-                    }
-                    break;
-                }
+        int l = 0;
+        int r = 0;
+        int cnt = 0;
+
+        while(r < n) {
+            if(s[r] == '1') {
+                cnt++;
             }
+            while(cnt == k) {
+                string str = s.substr(l, r-l+1);
+                if(ans.length() == 0 || 
+                    str.length() < ans.length() || 
+                    (str.length() == ans.length() && str < ans)) {
+                    ans = str;
+                }
+
+                if(s[l] == '1') cnt--;
+                l++;
+            }
+
+            r++;
         }
+
+        return ans;
+
+
+
+
+
+
+
+        // int n = s.length();
+        // string ans = "";
+        // for(int i=0; i<n; i++) {
+        //     string str = "";
+        //     int cnt = 0;
+        //     for(int j=i; j<n; j++) {
+        //         str.push_back(s[j]);
+        //         if(s[j] == '1') cnt++;
+        //         if(cnt == k) {
+        //             if(ans.length() == 0 || 
+        //                 str.length() < ans.length() || 
+        //                 (str.length() == ans.length() && str < ans)) {
+        //                 ans = str;
+        //             }
+        //             break;
+        //         }
+        //     }
+        // }
 
         return ans;
     }
