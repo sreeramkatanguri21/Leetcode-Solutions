f8e24a3740a2f4de068be81704ab3838bb07ddb9
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-09-09 20:45:36 +0530

    Time: 0 ms (100%), Space: 8.9 MB (99.2%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -3,7 +3,6 @@ public:
     long long countCommas(long long n) {
         long long ans = 0;
         long long val = 999;
-        int i = 1;
         while(n > val) {
             ans += (n-val);
             val = val*1000 + 999;
