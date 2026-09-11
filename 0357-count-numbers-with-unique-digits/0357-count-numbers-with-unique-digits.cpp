6222ddacadbf150073eaaef742528bccade6345c
sreeramkatanguri21 <sreeram.k2111@gmail.com>
2026-09-11 23:21:02 +0530

    Time: 0 ms (100%), Space: 8 MB (9.05%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -12,10 +12,13 @@ public:
         }
         return res;
     }
+    int dp[8][2][2][(1 << 10)];
     int solve(string& s, int idx, int tight, int lz, int mask, int n) {
         if(idx == s.length()) {
             return 1;
         }
+
+        if(dp[idx][tight][lz][mask] != -1) return dp[idx][tight][lz][mask];
 
         int lb = 0;
         int ub = (tight)?(s[idx]-'0'):9;
@@ -27,11 +30,12 @@ public:
             if(n == 0) cout << ans;
         }
 
-        return ans;
+        return dp[idx][tight][lz][mask] = ans;
     }
     int countNumbersWithUniqueDigits(int n) {
         ll val = pow(10, n)-1;
         string s = to_string(val);
+        memset(dp, -1, sizeof(dp));
         return solve(s, 0, 1, 1, 0, n);
     }
 };
